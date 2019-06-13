5f19d65c5ed322363bd7d7cc1c86df244c841ebe
Ian Alexander <iralex255@gmail.com>
2019-06-13 16:41:45 -0700

    create function to put intputted data in unordered list

--- groceryList/js/scripts.js
+++ groceryList/js/scripts.js
@@ -2,29 +2,11 @@ $(document).ready(function() {
   $("#blanks form").submit(function(event) {
     var blanks = ["item1", "item2", "item3", "item4", "item5"];
 
-    var inputs = []
-    blanks.forEach(function(blank) {
+    blanks.forEach(function(blank){
       var userInput = $("input#" + blank).val();
-      inputs.push(userInput.toUpperCase());
-
-      // var ordered = []
-      // inputs.map(function(input) {
-      //   return input.toUpperCase();
-      // });
+      $("." + blank).text(userInput);
     });
 
-debugger
-    // blanks.forEach(function(blank) {
-    //   var userInput = $("input#" + blank).val();
-    //   userInput.push(blank);
-    // });
-
-      // var orderedBlanks = blanks.map(function(blank) {
-      //   return blank.toUpperCase();
-
-        $("." + blank).text(userInput);
-      // });
-debugger
     $("#list").show();
 
   event.preventDefault();
